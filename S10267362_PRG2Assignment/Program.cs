783459d5a82afb162bb3dcd35b1fd3e7869b9ecc
zcloy0828 <s10267362@connect.np.edu.sg>
2025-01-28 09:38:29 +0800

    added feature two

--- S10267362_PRG2Assignment/Program.cs
+++ S10267362_PRG2Assignment/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+
+
+Dictionary<string, Flight> flights = new Dictionary<string, Flight>();
+void LoadFlights()
+{
+    using (StreamReader sr = new StreamReader("flights.csv"))
+    {
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            string[] parts = line.Split(',');
+            string flightNumber = parts[0];
+            string origin = parts[1];
+            string destination = parts[2];
+            DateTime expectedTime;
+            if (!DateTime.TryParse(parts[3], out expectedTime))
+            {
+                // Log or handle the invalid DateTime string
+                continue;
+            }
+            string status = parts[4];
+
+            DDJBFlight flight = new DDJBFlight(flightNumber, origin, destination, expectedTime, status);
+            flights.Add(flightNumber, flight);
+        }
+    }
+}
