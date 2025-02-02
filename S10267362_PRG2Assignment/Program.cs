b2f2db2ab455366f0984fc625c217173d3099d4f
zcloy0828 <s10267362@connect.np.edu.sg>
2025-02-02 13:55:07 +0800

    got got mashed up deleting everyhting now

--- S10267362_PRG2Assignment/Program.cs
+++ S10267362_PRG2Assignment/Program.cs
@@ -5,66 +5,6 @@
 
 
 
-void LoadFlights(Dictionary<string, Flight> flights, Terminal terminal)
-{
-    using (StreamReader sr = new StreamReader("flights.csv"))
-
-void LoadAirlines()
-{
-    using (StreamReader sr = new StreamReader("airlines.csv"))
-
-    {
-        sr.ReadLine();
-        string line;
-        while ((line = sr.ReadLine()) != null)
-        {
-            string[] parts = line.Split(',');
-
-            string flightNumber = parts[0];
-            string origin = parts[1];
-            string destination = parts[2];
-            DateTime expectedTime = DateTime.Parse(parts[3]);
-            string requestCode = parts[4];
-
-            if (requestCode == "DDJB")
-            {
-                Flight addFlight = new DDJBFlight(flightNumber, origin, destination, expectedTime);
-
-                flights.Add(flightNumber, addFlight);
-                terminal.GetAirlineFromFlight(addFlight).AddFlight(addFlight);
-            }
-            else if (requestCode == "CFFT")
-            {
-                Flight addFlight = new CFFTFlight(flightNumber, origin, destination, expectedTime);
-                flights.Add(flightNumber, addFlight);
-                terminal.GetAirlineFromFlight(addFlight).AddFlight(addFlight);
-            }
-            else if (requestCode == "LWTT")
-            {
-                Flight addFlight = new LWTTFlight(flightNumber, origin, destination, expectedTime);
-                flights.Add(flightNumber, addFlight);
-                terminal.GetAirlineFromFlight(addFlight).AddFlight(addFlight);
-            }
-            else
-            {
-                Flight addFlight = new NORMFlight(flightNumber, origin, destination, expectedTime);
-                flights.Add(flightNumber, addFlight);
-                terminal.GetAirlineFromFlight(addFlight).AddFlight(addFlight);
-            }
-        }
-    }
-}
-
-
-            string airlineCode = parts[1];
-            string airlineName = parts[0];
-            ;
-            // adding airline object into a dictionary
-            Airline airline = new Airline(airlineName, airlineCode);
-            terminal5.Airlines.Add(airlineCode, airline);
-        }
-    }
-}
 
 
 
