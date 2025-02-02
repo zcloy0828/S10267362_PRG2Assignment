424ec891fc3ddab7e09879fc01a039e2024ff20c
zcloy0828 <s10267362@connect.np.edu.sg>
2025-02-02 13:38:50 +0800

    remove the object cause it will cause conflict

--- S10267362_PRG2Assignment/Program.cs
+++ S10267362_PRG2Assignment/Program.cs
@@ -24,11 +24,5 @@ void LoadAirlines()
     }
 }
 
-using S10267362_PRG2Assignment;
 
-Terminal terminal5 = new Terminal("Terminal 5");
-Dictionary<string, Flight> flights = new Dictionary<string, Flight>();
-LoadAirlines();
-LoadBoardingGates(terminal5.BoardingGates);
-LoadFlights(flights, terminal5);
 
