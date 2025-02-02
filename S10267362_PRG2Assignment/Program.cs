524a643ef2230ddd2f56c839d2a771c6c8e11ab0
zcloy0828 <s10267362@connect.np.edu.sg>
2025-02-02 13:33:16 +0800

    objects

--- S10267362_PRG2Assignment/Program.cs
+++ S10267362_PRG2Assignment/Program.cs
@@ -1,1 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+using S10267362_PRG2Assignment;
+
+Terminal terminal5 = new Terminal("Terminal 5");
+Dictionary<string, Flight> flights = new Dictionary<string, Flight>();
+LoadAirlines();
+LoadBoardingGates(terminal5.BoardingGates);
+LoadFlights(flights, terminal5);
