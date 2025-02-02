12f1b88088478375e713b236c36bb32470101a9b
zcloy0828 <s10267362@connect.np.edu.sg>
2025-02-02 13:34:32 +0800

    adding feature 1 to master

--- S10267362_PRG2Assignment/Program.cs
+++ S10267362_PRG2Assignment/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+
+void LoadAirlines()
+{
+    using (StreamReader sr = new StreamReader("airlines.csv"))
+    {
+        sr.ReadLine();
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            string[] parts = line.Split(',');
+
+            string airlineCode = parts[1];
+            string airlineName = parts[0];
+            ;
+            // adding airline object into a dictionary
+            Airline airline = new Airline(airlineName, airlineCode);
+            terminal5.Airlines.Add(airlineCode, airline);
+        }
+    }
+}
